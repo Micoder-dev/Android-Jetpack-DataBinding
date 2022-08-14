c600a1c7607907892ef29af71f1dfa0893f878c2
Micoder-dev <micoder.com@gmail.com>
2022-08-14 13:19:12 +0530

    Data Binding for Event Handling "Buttons"

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.databinding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mActivityMainBinding;
+    private MainActivityClickHandlers onClickHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +22,32 @@ public class MainActivity extends AppCompatActivity {
 
         mActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        //Text View
         PersonModel personModel = new PersonModel("Micoder", "micoder.com@gmail.com");
-
         mActivityMainBinding.setPerson(personModel);
 
+        //Binding the handler
+        onClickHandler = new MainActivityClickHandlers(this);
+        mActivityMainBinding.setOnClickHandler(onClickHandler);
+
     }
+
+    public class MainActivityClickHandlers{
+
+        Context mContext;
+
+        public MainActivityClickHandlers(Context context) {
+            mContext = context;
+        }
+
+        public void onHelloBtnClick(View view) {
+            Toast.makeText(mContext, "Hello btn clicked", Toast.LENGTH_SHORT).show();
+        }
+        
+        public void onByeBtnClick(View view) {
+            Toast.makeText(mContext, "Bye btn clicked", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
