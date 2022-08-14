142b0e128eb462d484ef8f41c8ee96642f6c8cd0
Micoder-dev <micoder.com@gmail.com>
2022-08-14 13:00:53 +0530

    Data Binding for TextViews

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.databinding.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mActivityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        PersonModel personModel = new PersonModel("Micoder", "micoder.com@gmail.com");
+
+        mActivityMainBinding.setPerson(personModel);
+
     }
 }
---
+++ app/src/main/java/com/example/databinding/PersonModel.java
@@ -0,0 +1,27 @@
+package com.example.databinding;
+
+public class PersonModel {
+
+    String name,email;
+
+    public PersonModel(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
